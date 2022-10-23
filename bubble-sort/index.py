602bb4ff04226b2916330a8596d6fac2af142b5b
Akalanka <akalankaperera128@gmail.com>
2022-10-23 21:53:15 +0530

    Added python bubble sort

---
+++ bubble-sort/index.py
@@ -0,0 +1,18 @@
+def sort(arr, direction="asc"):
+    noMoreSwaps = False
+    n = 1
+    while not noMoreSwaps:
+        noMoreSwaps = True
+        for i in range(0, len(arr) - n):
+            condition = (arr[i] > arr[i + 1]) if direction == 'asc' else (arr[i] < arr[i + 1])
+            if condition:
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                noMoreSwaps = False
+        n += 1
+    return arr
+
+
+numbers = [3, 4, 5, 6, 2, 3, 1, 98, 4, 324, 13, 2, 3]
+print(sort(numbers))
